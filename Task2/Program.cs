54a545bb14462bc93a9076315c4b025f1b32e7a8
Macbook M2 <macbookm2@MacBook-Air-Macbook.local>
2025-04-15 10:50:56 +0300

    Первый коммит

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите четырехзначное число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number >= 1000 && number <= 9999)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            Console.WriteLine("Сумма цифр числа: " + sum);
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: необходимо ввести четырехзначное число.");
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите двузначное число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number >= 10 && number <= 99)
+        {
+            int firstDigit = number / 10; // Первая цифра
+            int lastDigit = number % 10;  // Последняя цифра
+
+            Console.WriteLine("Первая цифра: " + firstDigit);
+            Console.WriteLine("Последняя цифра: " + lastDigit);
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: необходимо ввести двузначное число.");
+        }
+    }
+}
