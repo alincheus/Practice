ffbbb6b1a6eb44648232cb0aa7ae877cbb3f7b9b
Macbook M2 <macbookm2@MacBook-Air-Macbook.local>
2025-04-17 09:41:15 +0300

    Восстановление файлоф

---
+++ Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите целое число N (1 <= N <= 10):");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        if (N >= 1 && N <= 10)
+        {
+            int sum = 0;
+
+            for (int i = N; i <= 2 * N; i++)
+            {
+                sum += i * i; 
+            }
+
+            Console.WriteLine($"Сумма квадратов от {N} до {2 * N}: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: число N должно быть в диапазоне от 1 до 10.");
+        }
+    }
+}
