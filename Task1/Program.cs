5307652e2db5cfda265d254f398ee79ce5952c7c
Macbook M2 <macbookm2@MacBook-Air-Macbook.local>
2025-04-17 10:45:41 +0300

    Организация

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое целое число:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите второе целое число:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = number1 + number2;
+
+        Console.WriteLine("Сумма введенных чисел: " + sum);
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 3.5;
+
+        double cosSquared = Math.Pow(Math.Cos(x), 2);
+        double sqrtXCube = Math.Sqrt(Math.Pow(x, 3));
+        double expLn2X = Math.Exp(Math.Log(2 * x));
+        double denominator = Math.Sin(x) + expLn2X;
+
+        double y = cosSquared - (sqrtXCube + 1) / denominator;
+
+        Console.WriteLine("Значение функции y: " + y);
+    }
+}
