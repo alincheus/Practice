5dfd4c3e0ca565d854078683ecb10a53a24613a9
Macbook M2 <macbookm2@MacBook-Air-Macbook.local>
2025-04-17 09:43:54 +0300

    Исправлены конфликты перед push

--- Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63057e7aa3181aa29ef371d4d06465bbcae2d373")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffbbb6b1a6eb44648232cb0aa7ae877cbb3f7b9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
