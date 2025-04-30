9e56e308a8882a048454ed08170ed291faf52a99
Macbook M2 <macbookm2@MacBook-Air-Macbook.local>
2025-04-30 14:27:47 +0300

    Add Lab9

---
+++ Lab9/Task1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+struct STUDENT
+{
+    public string Name;     
+    public int GroupNumber;   
+    public int[] Grades;      
+
+    public double GetAverageGrade()
+    {
+        return Grades.Average();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        const int studentCount = 10;
+        STUDENT[] students = new STUDENT[studentCount];
+
+        Console.WriteLine("Введите данные для 10 студентов:");
+
+        for (int i = 0; i < studentCount; i++)
+        {
+            Console.WriteLine($"\nСтудент {i + 1}:");
+
+            Console.Write("Фамилия и инициалы: ");
+            students[i].Name = Console.ReadLine();
+
+            Console.Write("Номер группы: ");
+            students[i].GroupNumber = int.Parse(Console.ReadLine());
+
+            students[i].Grades = new int[5];
+            Console.WriteLine("Введите 5 оценок:");
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write($"Оценка {j + 1}: ");
+                students[i].Grades[j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        students = students.OrderBy(s => s.GroupNumber).ToArray();
+
+        Console.WriteLine("\nСтуденты с средним баллом больше 4.0:");
+        bool found = false;
+
+        foreach (var student in students)
+        {
+            if (student.GetAverageGrade() > 4.0)
+            {
+                Console.WriteLine($"Фамилия: {student.Name}, Номер группы: {student.GroupNumber}");
+                found = true;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Нет студентов со средним баллом выше 4.0.");
+        }
+    }
+}
---
+++ Lab9/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+enum Post
+{
+    Manager = 160,   
+    Engineer = 150,  
+    Clerk = 140,     
+    Intern = 120    
+}
+
+class Accauntant
+{
+    public bool AskForBonus(Post worker, int hours)
+    {
+        int requiredHours = (int)worker; 
+        return hours > requiredHours;   
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Accauntant accauntant = new Accauntant();
+
+        Console.WriteLine("Введите должность (Manager, Engineer, Clerk, Intern):");
+        string postInput = Console.ReadLine();
+
+        Console.WriteLine("Введите количество отработанных часов:");
+        int workedHours = int.Parse(Console.ReadLine());
+
+        if (Enum.TryParse(postInput, true, out Post worker))
+        {
+            bool isBonusGiven = accauntant.AskForBonus(worker, workedHours);
+            Console.WriteLine(isBonusGiven
+                ? "Сотруднику положена премия."
+                : "Сотруднику не положена премия.");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: некорректная должность.");
+        }
+    }
+}
