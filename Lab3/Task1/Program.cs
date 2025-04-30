6ea17e51d46f5983892f3238cf8753f0cd23b9fd
Macbook M2 <macbookm2@MacBook-Air-Macbook.local>
2025-04-30 14:23:31 +0300

    Add Lab3

---
+++ Lab3/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class A
+{
+    public int a { get; private set; }
+    public int b { get; private set; }
+
+    public A(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int Sum()
+    {
+        return a + b;
+    }
+
+    public double CalculateExpression()
+    {
+        return Math.Sin(b) / (3 * a);
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Значение a: {a}");
+        Console.WriteLine($"Значение b: {b}");
+        Console.WriteLine($"Сумма a и b: {Sum()}");
+        Console.WriteLine($"Значение выражения: {CalculateExpression():F6}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значения a и b:");
+        Console.Write("a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        A obj = new A(a, b);
+
+        obj.DisplayInfo();
+    }
+}
---
+++ Lab3/Task3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+class Counter
+{
+    private int value;  
+    private int min;    
+    private int max;    
+
+    public Counter() : this(0, 100, 0) { }
+
+    public Counter(int min, int max, int startValue)
+    {
+        if (min > max || startValue < min || startValue > max)
+        {
+            throw new ArgumentException("Некорректные значения диапазона!");
+        }
+
+        this.min = min;
+        this.max = max;
+        this.value = startValue;
+    }
+    
+    public void Increase()
+    {
+        if (value < max)
+        {
+            value++;
+        }
+        else
+        {
+            Console.WriteLine("Достигнуто максимальное значение.");
+        }
+    }
+
+    public void Decrease()
+    {
+        if (value > min)
+        {
+            value--;
+        }
+        else
+        {
+            Console.WriteLine("Достигнуто минимальное значение.");
+        }
+    }
+
+    public int CurrentValue => value;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите минимальное и максимальное значения счетчика:");
+        Console.Write("Минимальное значение: ");
+        int min = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Максимальное значение: ");
+        int max = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Начальное значение: ");
+        int start = Convert.ToInt32(Console.ReadLine());
+
+        try
+        {
+            Counter counter = new Counter(min, max, start);
+
+            Console.WriteLine("Текущее значение счетчика: " + counter.CurrentValue);
+            Console.WriteLine("Увеличиваем счетчик...");
+            counter.Increase();
+            Console.WriteLine("Текущее значение: " + counter.CurrentValue);
+            Console.WriteLine("Уменьшаем счетчик...");
+            counter.Decrease();
+            Console.WriteLine("Текущее значение: " + counter.CurrentValue);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+}
