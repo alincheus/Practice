8900a26f804bc71ec3278f094c34dad7347bdfa6
Macbook M2 <macbookm2@MacBook-Air-Macbook.local>
2025-04-30 14:29:04 +0300

    Add Lab11

---
+++ Lab11/Task1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+class Apartment 
+{
+    private string name;          
+    private double pricePerSquareMeter; 
+    private double area;          
+
+    public Apartment(string name, double pricePerSquareMeter, double area)
+    {
+        this.name = name;
+        this.pricePerSquareMeter = pricePerSquareMeter;
+        this.area = area;
+    }
+
+    public string GetName() => name;
+    public double GetPricePerSquareMeter() => pricePerSquareMeter;
+    public double GetArea() => area;
+
+    public virtual double CalculateCost()
+    {
+        return pricePerSquareMeter * area;
+    }
+
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"Название: {name}");
+        Console.WriteLine($"Стоимость 1 м²: {pricePerSquareMeter}");
+        Console.WriteLine($"Площадь: {area}");
+        Console.WriteLine($"Стоимость квартиры: {CalculateCost()}");
+    }
+}
+
+class CentralApartment : Apartment 
+{
+    private string districtName; 
+
+    public CentralApartment(string name, double pricePerSquareMeter, double area, string districtName)
+        : base(name, pricePerSquareMeter, area)
+    {
+        this.districtName = districtName;
+    }
+
+    public override double CalculateCost()
+    {
+        double baseCost = base.CalculateCost();
+        return baseCost + (baseCost * 0.01); 
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Название района: {districtName}");
+        Console.WriteLine($"Стоимость с учетом надбавки: {CalculateCost()}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Apartment basicApartment = new Apartment("Квартира у парка", 1200, 55);
+        CentralApartment centralApartment = new CentralApartment("Квартира в центре", 2000, 70, "Центральный район");
+
+        Console.WriteLine("Базовый класс:");
+        basicApartment.DisplayInfo();
+
+        Console.WriteLine("\nПроизводный класс:");
+        centralApartment.DisplayInfo();
+
+        Apartment[] apartments = new Apartment[3];
+        apartments[0] = basicApartment;
+        apartments[1] = centralApartment;
+        apartments[2] = new CentralApartment("Квартира у площади", 2500, 80, "Площадь Победы");
+
+        Console.WriteLine("\nПолиморфный вывод всех объектов:");
+        foreach (var apartment in apartments)
+        {
+            apartment.DisplayInfo();
+            Console.WriteLine("--------------------------");
+        }
+    }
+}
---
+++ Lab11/Task2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class A
+{
+    private int a; 
+    private int b; 
+
+    public int C
+    {
+        get { return a + b; }
+    }
+
+    public A()
+    {
+        a = 5; 
+        b = 10; 
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Класс A: a = {a}, b = {b}, c = {C}");
+    }
+}
+
+class B : A
+{
+    private int d; 
+
+    public int C2
+    {
+        get
+        {
+            int sum = 0;
+            int temp = d;
+            do
+            {
+                sum += temp;
+                temp--;
+            } while (temp > 0);
+            return C + sum;
+        }
+    }
+
+    public B() : base()
+    {
+        d = 3; 
+    }
+
+    public B(int dValue) : base()
+    {
+        d = dValue;
+    }
+
+    public new void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Класс B: d = {d}, c2 = {C2}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        A objA = new A();
+        Console.WriteLine("Объект класса A:");
+        objA.DisplayInfo();
+
+        B objB1 = new B();
+        Console.WriteLine("\nОбъект класса B (наследуемый конструктор):");
+        objB1.DisplayInfo();
+
+        B objB2 = new B(5);
+        Console.WriteLine("\nОбъект класса B (собственный конструктор):");
+        objB2.DisplayInfo();
+    }
+}
